e483a482d7656c198dc72ff6b99b823183584bb3
SolomonB127 <solomonakinsulire@gmail.com>
2023-12-25 21:48:19 +0000

    Reformatted the server.js file(added hostId to the array of objects), Built the vansHost page

--- src/assets/Components/Pages/Host/VansHost.jsx
+++ src/assets/Components/Pages/Host/VansHost.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
-
+import React from 'react';
+import './stylesheets/VansHost.css';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 const VansHost = () => {
+  const [vanHost, setVanHost] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/host/vans")
+      .then(res => res.json())
+        .then(data => setVanHost(data.vans))
+  }, []);
+
+  const listElements = vanHost.map(van => (
+    <Link to={`/host/vanshost/${van.id}`} key={van.id}  className="host-van-link-wrapper">
+
+      <div className="host-van-single" key={van.id}>
+        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+        <div className="host-van-info">
+          <h3>{van.name}</h3>
+            <p>${van.price}/day</p>
+        </div>
+      </div>
+    </Link>
+  ))
   return (
-    <div>VansHost</div>
+    <section>
+     <h1 className='host-vans-title'>Your listed vans here</h1>
+     <div className='host-vans-list'>
+      {
+        vanHost.length > 0 ? (
+          <section>
+            {listElements}
+          </section>
+        ) : (
+          <h2>Loading...</h2>
+        )
+      }
+     </div>
+    </section>
   )
 }
 
