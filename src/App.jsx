3e0d9b06dc7b80cd24ab2adb6be35ba0ba7a260c
SolomonB127 <solomonakinsulire@gmail.com>
2023-12-25 16:17:28 +0000

    Reformatted the Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,16 @@ function App() {
     <>
     <div className='App'>
       <Routes>
-        <Route element={ <Layout /> }>
-          <Route path='/'  element={ <Home /> } />
-          <Route path='/about'  element={ <About /> } />
-          <Route path='/van'  element={ <Vans/> } />
-          <Route path='/van/:id'  element={ <VansDetails/> } />
+        <Route path='/' element={ <Layout /> }>
+          <Route  index element={ <Home /> } />
+          <Route path='about'  element={ <About /> } />
+          <Route path='van'  element={ <Vans/> } />
+          <Route path='van/:id'  element={ <VansDetails/> } />
           
-          <Route path='/host'  element={ <HostLayout/> } >
-            <Route path='/host/income'  element={ <Income/> } />
-            <Route path='/host/reviews'  element={ <Reviews/> } />
+          <Route path='host'  element={ <HostLayout/> } >
+            <Route index element={ <Dashboard /> } />
+            <Route path='income'  element={ <Income/> } />
+            <Route path='reviews'  element={ <Reviews/> } />
           </Route>
         </Route>
       </Routes>
